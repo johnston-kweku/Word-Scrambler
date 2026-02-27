afd09e0f49a630eabb2e6d4d94036ff544e32615
Johnston Kweku <johnstonkweku@gmail.com>
2026-02-27 16:39:44 +0000

    Fix PWA paths for GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = "word-scrambler-v2";
+const CACHE_NAME = "word-scrambler-v3";
 
 self.addEventListener("install", event => {
 event.waitUntil(
 caches.open(CACHE_NAME).then(cache => {
 return cache.addAll([
-"/Word-Scrambler/",
-"/Word-Scrambler/index.html",
-"/Word-Scrambler/output.css",
-"/Word-Scrambler/javascript/index.js"
+  "/Word-Scrambler/",
+  "/Word-Scrambler/index.html",
+  "/Word-Scrambler/output.css",
+  "/Word-Scrambler/javascript/index.js"
 ]);
 })
 );
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
-);
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match("/Word-Scrambler/index.html");
+    })
+  );
 });
